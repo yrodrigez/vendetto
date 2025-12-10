840f0c49c2174fa126f3e7eba58a364cbaad1396
Yago <yarlorenzo@gmail.com>
2025-12-10 16:30:48 +0100

    hot-fix: fixed POSTGRES_DATABASE from POSTGRES_DB

--- src/infrastructure/environment/index.ts
+++ src/infrastructure/environment/index.ts
@@ -10,7 +10,7 @@ const environment = {
     postgres: {
         host: process.env.POSTGRES_HOST!,
         port: process.env.POSTGRES_PORT!,
-        database: process.env.POSTGRES_DB!,
+        database: process.env.POSTGRES_DATABASE!,
         user: process.env.POSTGRES_USER!,
         password: process.env.POSTGRES_PASSWORD!,
         ssl: process.env.POSTGRES_SSL === 'true'
