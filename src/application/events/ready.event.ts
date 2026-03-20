e58114ac6cb1e7087b8a9e5c9c92bf4fbd9fe44e
Yago <yarlorenzo@gmail.com>
2026-03-20 22:30:54 +0100

    fix(ready-event): added an environment check to send the ready evnet log message

--- src/application/events/ready.event.ts
+++ src/application/events/ready.event.ts
@@ -1,10 +1,13 @@
 import { GuildFeaturePolicyService } from "@/application/features/guild-feature-policy.service";
 import { DiscordEvent } from "@/infrastructure/discord/events/event.interface";
+import { getEnvironment } from "@/infrastructure/environment";
 import { ChannelType, Client, ClientEvents, Events, TextChannel } from 'discord.js';
 
 export class ReadyEvent implements DiscordEvent {
     public readonly name: keyof ClientEvents = Events.ClientReady;
     public readonly once = true;
+
+    public readonly configuration = getEnvironment();
 
     constructor(
         private readonly featuresService: GuildFeaturePolicyService,
@@ -27,8 +30,8 @@ export class ReadyEvent implements DiscordEvent {
                 return channel.type === ChannelType.GuildText && (channel.name === 'vendetto-system');
             }) as TextChannel | undefined;
 
-            if (channel && channel.send) {
-                //await channel.send("Vendetto ready to serve!");
+            if (channel?.send && this.configuration.environment === 'production') {
+                await channel.send("Vendetto ready to serve!");
                 console.log(`Sent ready message to channel ${channel.name} (${channel.id}) in guild ${guild.name} (${guild.id})`);
             } else {
                 console.log(`No channel found in guild ${guild.id}`);
